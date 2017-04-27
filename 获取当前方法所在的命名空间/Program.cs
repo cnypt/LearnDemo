8c359abd88e65e1a0224d2e5aa09d45ec0bc68b8
PengtaoY <1197726544@qq.com>
2017-04-27 10:45:12 +0800

    添加项目文件。

---
+++ 获取当前方法所在的命名空间/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace 获取当前方法所在的命名空间
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(GetMethodInfo());
+
+            Console.ReadKey();
+        }
+
+        private static string GetMethodInfo()
+        {
+            string str = "";
+            //取得当前方法命名空间
+            str += "命名空间名:" + System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Namespace + "\n";
+            //取得当前方法类全名
+            str += "类名:" + System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName + "\n";
+            //取得当前方法名
+            str += "方法名:" + System.Reflection.MethodBase.GetCurrentMethod().Name + "\n";
+            str += "\n";
+
+            StackTrace ss = new StackTrace(true);
+            MethodBase mb = ss.GetFrame(1).GetMethod();
+            //取得父方法命名空间
+            str += mb.DeclaringType.Namespace + "\n";
+            //取得父方法类名
+            str += mb.DeclaringType.Name + "\n";
+            //取得父方法类全名
+            str += mb.DeclaringType.FullName + "\n";
+            //取得父方法名
+            str += mb.Name + "\n";
+            return str;
+        }
+    }
+}
